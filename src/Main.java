9d9f3e796ee53ebe750a0db25ea4696c862ca57a
abatonisandra <abatonisandra@gmail.com>
2025-09-18 08:14:54 +0200

    calculate the area

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class ShapeCalculator{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Choose a shape to calculate area:");
+        System.out.println("1 = Circle");
+        System.out.println("2 = Rectangle");
+        System.out.println("3 = Triangle");
+        System.out.println("Enter your choice: ");
+        int choice = scanner.nextInt();
+
+        Shape shape = null;
+        switch (choice){
+            case 1:
+                System.out.print("Enter radius of the circle: ");
+                double radius = scanner.nextDouble();
+                shape = new Circle(radius);
+                break;
+
+            case 2:
+                System.out.print("Enter length of the rectangle: ");
+                double length = scanner.nextDouble();
+                System.out.print("Enter width of the rectangle: ");
+                double width = scanner.nextDouble();
+                shape = new Rectangle(lengrh, width);
+                break;
+
+            case 3:
+                System.out.print("Enter base of the triangle");
+
         }
     }
 }
--- src/Shape.java
+++ src/Shape.java
@@ -1,2 +1,39 @@
-public class Shape {
+abstract class Shape {
+abstract double calculateArea();
 }
+class circle extends shape{
+    private double radius;
+
+    public circle(double radius){
+        this.radius = radius;
+    }
+    @Override
+    double calculateArea(){
+        return Math.PI * radius * radius;
+    }
+}
+class Rectangle extends Shape{
+    private double length, width;
+
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+    @Override
+    double calculateArea(){
+        return length * width;
+    }
+}
+class triangle extends Shape{
+    private double base, height;
+
+    public Triangle(double base, double height){
+        this.base = base;
+        this.height = height;
+    }
+    @Override
+    double calculateArea(){
+        return 0.5 * base * height;
+    }
+}
+
